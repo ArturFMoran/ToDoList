8a03a2d24ea2089a76dc94f05ad565cf52ed5197
Artur Felipe Moran <aobayojimbo@gmail.com>
2023-08-15 14:30:35 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Artur Felipe Moran on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
